03063ecd0e7dfeab057c88ad5c58c23bf14200ae
Andreas Jagelund <andreas@oh77.se>
2025-10-14 23:20:49 +0200

    update page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Hockey Matcher",
-  description: "SHL och SDHL matchinformation",
+  title: "Gameday",
+  description: "CHL, SHL och SDHL matchinformation",
 };
 
 export default function RootLayout({
